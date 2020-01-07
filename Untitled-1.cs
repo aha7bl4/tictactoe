73eaa43c3793e6b88dc94d9758e7e4315c9c7b8e
aha7bl4 <akha_kh@mail.ru>
2020-01-07 13:15:09 +0300

    Beta version of tictactoe

---
+++ Untitled-1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Projectone
+{
+   class MainClass
+   {
+      public static void Main (string[] args)
+      {
+
+          Console.WriteLine("dfjkvx")
+      }
+
+   }
+
+
+}
